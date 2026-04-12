5b36d06d4789090ac21412628f136c3068331bf0
CatBoi1107 <therealjohmsith1256@gmail.com>
2026-04-12 20:00:33 +0700

    Forgot the changes

--- init.lua
+++ init.lua
@@ -161,6 +161,8 @@ vim.o.scrolloff = 10
 
 -- personal configs
 vim.opt.relativenumber = true
+vim.opt.expandtab = false
+vim.opt.shiftwidth = 2
 
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s)
