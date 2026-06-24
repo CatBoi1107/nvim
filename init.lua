5251d3e5eda8a27564e1f72f46a94e82769ce728
CatBoi1107 <therealjohnsmith1256@gmail.com>
2026-06-24 12:25:48 +0700

    feat/numberline

--- init.lua
+++ init.lua
@@ -3,5 +3,9 @@ vim.opt.clipboard = 'unnamedplus'
 -- Set <leader> to spacebar
 vim.g.mapleader = " "
 
+-- Number line and relative number line
+vim.opt.number = true
+vim.opt.relativenumber = true
+
 require("config.lazy")
 require("mappings")
